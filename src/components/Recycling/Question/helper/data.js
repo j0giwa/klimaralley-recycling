6b792a9b8045340e80503e96761bab321962e205
Josy-237 <tanga.josiane@yahoo.fr>
2024-05-23 21:57:27 +0200

    data js ergänzen

--- src/components/Recycling/Question/helper/data.js
+++ src/components/Recycling/Question/helper/data.js
@@ -4,7 +4,7 @@ export const data =[
     {
         id:1,
         img:'https://www.oekotest.de/static_files/images/article/Elektroschrott-entsorgen-Grosser-Ratgeber_Shutterstock-Sergey-Mironov_11536_4x3.jpg',
-        frage:'Für Elektronikgeräte agufkfjafjoaffhaguhagu hasgiaund Batterien gibt es eine eigene Mülltonne.',
+        frage:'Für Elektronikgeräte und Batterien gibt es eine eigene Mülltonne.',
         buton1:'Ja',
         buton2:'Nein'
         
@@ -48,6 +48,139 @@ export const data =[
         buton3:'Nach ihren Formen'
         
     },
+    {
+        id:6,
+        image:"https://images.cdn.sofatutor.net/content_images/images/12135/original/20466_muelltrennung_haushalt.svg?1648709784",
+        title:"Warum trennen wir Müll überhaupt?",
+        answers:[
+            {
+                id:0,
+                title:"Damit der Müll wiederverwendet werden kann",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:1,
+                title:"Weil die UNO ein entsprechendes Gesetz verabschiedet, hat",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:2,
+                title:"Weil z.B Kompost im Altpapiercontainer stinken würde",
+                backgroundColor:"#32a852",
+
+            },
+        ],
+
+        correctAnswerId: 0,
+    }, {
+        id:7,
+        image:"https://i.ds.at/WzGm6A/rs:fill:1600:0/plain/2023/01/15/grafik-gelbe-tonne_1.jpg",
+        title:"Welche von diese 3 produkt daft mann nicht in den HausMüll werfen?",
+        answers:[
+            {
+                id:0,
+                title:"Altpapier",
+                backgroundColor:"#fcba03",
+
+            },
+            {
+                id:1,
+                title:"Batterien",
+                backgroundColor:"#fcba03",
+
+            },
+            {
+                id:2,
+                title:"Kompost",
+                backgroundColor:"#fcba03",
+
+            },
+        ],
+        correctAnswerId: 1,
+
+    },  {
+        id:8,
+        image:"https://www.verbraucherzentrale.nrw/sites/default/files/styles/article_full_image_desktop/public/2018-12/teaserbild2.jpg?h=d40193ab&itok=-ecTqt45",
+        title:"Welche Farbe hat die Tonne, wo man das Verpackungsmaterial entsorgt?",
+        answers:[
+            {
+                id:0,
+                title:"blau",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:1,
+                title:"gelb",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:2,
+                title:"grün",
+                backgroundColor:"#32a852",
+
+            },
+        ],
+
+        correctAnswerId: 0,
+    },
+    {
+        id:9,
+        image:"https://www.muelltrennung-wirkt.de/fileadmin/user_upload/Startseite/dS_Illu_S4_carousel_Muelltrennung_blau.svg",
+        title:"welcher Müll gehört in die blaue Tonne?",
+        answers:[
+            {
+                id:0,
+                title:"Altpapier",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:1,
+                title:"Glas",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:2,
+                title:"Kompost",
+                backgroundColor:"#32a852",
+
+            },
+        ],
+        correctAnswerId: 0,
+
+    },
+    {
+        id:10,
+        image:"https://www.resorti.de/blog/wp-content/uploads/2017/05/altglasentsorgung-glasrecycling-titel-resorti-blog.jpg",
+        title:"Glas ist wiederverwenbar, wie kann man Glas in Container noch trennen, nämlich nach….?",
+        answers:[
+            {
+                id:0,
+                title:"Farbe",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:1,
+                title:"Form",
+                backgroundColor:"#32a852",
+
+            },
+            {
+                id:2,
+                title:"Kompost",
+                backgroundColor:"#32a852",
+
+            },
+        ],
+        correctAnswerId: 1,
+
+    }, 
 
     
 ]
