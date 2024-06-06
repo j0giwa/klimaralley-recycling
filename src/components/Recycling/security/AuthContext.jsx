422e2760c6f43fe9ece69446e8f3d48760b92fcc
JBjeff <boettcher-jeffrey@t-online.de>
2024-06-06 10:11:19 +0200

    JWT Auth eingefügt

--- src/components/Recycling/security/AuthContext.jsx
+++ src/components/Recycling/security/AuthContext.jsx
@@ -1,4 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
+import {  executeJWTAuthenticationService } from "../api/AuthenticationApiService";
+import { apiClient } from "../api/ApiClient";
 
 export const AuthContext = createContext()
 
@@ -10,29 +12,71 @@ export default function AuthProvider({ children }) {
 
   const [username, setUsername] = useState(null)
 
-  function login(username, password) {
+  const [token, setToken] = useState(null)
 
-    if (username === 'Admin' && password === '1234') {
-      console.log('logged')
-      setAuthenticated(true)
-      setUsername(username)
-      return true
+  // function login(username, password) {
 
-    } else {
-      setAuthenticated(false)
-      setUsername(null)
+  //   if (username === 'Admin' && password === '1234') {
+  //     console.log('logged')
+  //     setAuthenticated(true)
+  //     setUsername(username)
+  //     return true
+
+  //   } else {
+  //     setAuthenticated(false)
+  //     setUsername(null)
+  //     return false
+  //   }
+  // }
+
+  async function login(username, password) {
+
+    try {
+
+      const response = await executeJWTAuthenticationService(username, password)
+      const jwtToken = 'Bearer ' + response.data.token
+      
+      if (response.status == 200) {
+        setAuthenticated(true)
+        setUsername(username)
+        setToken(jwtToken)
+
+        apiClient.interceptors.request.use(
+          (config) => {
+            console.log('intercepting and adding a token')
+            config.headers.Authorization = jwtToken
+            return config
+          }
+        )
+
+        return true
+
+      } else {
+        console.error('Es gab einen Fehler:', error.message);
+        logout()
+        return false
+
+      }
+    } catch (error) {
+      console.error('Es gab einen Fehler:', error.message);
+      console.error('Fehlerdetails:', error);
+      logout()
       return false
     }
   }
 
+
+
   function logout() {
     setAuthenticated(false)
+    setUsername(null)
+    setToken(null)
   }
 
   return (
-    
-    <AuthContext.Provider  value = {{ isAuthenticated, login, logout, username } }>
+
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, username, token }}>
       {children}
-      </AuthContext.Provider>
+    </AuthContext.Provider>
   )
 }
