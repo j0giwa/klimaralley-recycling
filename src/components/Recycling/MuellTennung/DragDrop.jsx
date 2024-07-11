8c5e8bc278c46cc8a98da3a3030d7b060f4130fd
px2023 <ghub@gmx-ist-cool.de>
2024-07-11 02:25:21 +0200

    fixed Button zum nächsten Spiel

--- src/components/Recycling/MuellTennung/DragDrop.jsx
+++ src/components/Recycling/MuellTennung/DragDrop.jsx
@@ -181,7 +181,7 @@ function DragDrop() {
   };
  //navigiert zum nächsten Spiel, in dem zu der Liste der Games
   const nextGame = () => {
-    navigate('/play/recycling/recyclebar');
+    navigate('/play/recycling/recyclebar/:id');
     
   };
   //Frontend ansicht
