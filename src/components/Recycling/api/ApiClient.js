2cf46db899bccc1c4b116c2b071b1b5b0f15a68e
JBjeff <boettcher-jeffrey@t-online.de>
2024-09-18 15:17:31 +0200

    API beschrieben

--- src/components/Recycling/api/ApiClient.js
+++ src/components/Recycling/api/ApiClient.js
@@ -1,9 +1,17 @@
 
 import axios from "axios";
 
+/**
+ * Autor: Jeffrey Böttcher
+ * 
+ * Beschreibung:
+ * Verbindet die Anwendung mit dem Backend-Server über eine HTTP-Anfragebibliothek.
+ **/
 
- export const apiClient = axios.create({
+// Erzeugt eine Instanz von 'axios' mit vordefinierten Konfigurationen
+export const apiClient = axios.create({
+    // Basis-URL für alle Anfragen, die mit dieser Instanz gesendet werden
     baseURL: 'http://localhost:8080'
     
-})
-
+    // Hier können weitere Konfigurationen hinzugefügt
+});
